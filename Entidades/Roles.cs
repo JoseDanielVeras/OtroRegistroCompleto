123260acad0f65029e760b81638c28bb31ab92f6
JoseDanielVeras <josedaniel510jd@gmail.com>
2021-02-03 21:40:51 -0400

    Agregando clase Roles

---
+++ Entidades/Roles.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+
+namespace OtroRegistroCompleto.Entidades
+{
+    class Roles
+    {
+        [Key]
+        public int RolId { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
