b618ce8e45456731dba63d4467cebc3019531532
JoseDanielVeras <josedaniel510jd@gmail.com>
2021-02-15 20:20:55 -0400

    Prueba

--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace OtroRegistroCompleto
         [STAThread]
         static void Main()
         {
-            Application.SetHighDpiMode(HighDpiMode.SystemAware);
+            Application.SetHighDpiMode(HighDpiMode.SystemAware);S
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new RegistroUsuariosForm());
